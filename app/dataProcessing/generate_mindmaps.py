b60e6183dfe36aad0ceacd65bc27e9f0fb71c03f
Romaissaafadal <151086756+Romaissaafadal@users.noreply.github.com>
2025-04-26 14:38:38 +0100

    Update generate_mindmaps.py

--- app/dataProcessing/generate_mindmaps.py
+++ app/dataProcessing/generate_mindmaps.py
@@ -1,13 +1,1 @@
-import matplotlib.pyplot as plt
-import networkx as nx
 
-def generer_carte_mentale(graphe, nom_fichier):
-    G = nx.Graph()
-    for lien in graphe:
-        G.add_edge(lien["source"], lien["cible"], weight=lien["poids"])
-    pos = nx.spring_layout(G)
-    plt.figure(figsize=(10, 8))
-    nx.draw(G, pos, with_labels=True, node_color="skyblue", font_size=10, node_size=1500, edge_color='gray')
-    plt.title("Carte mentale - concepts clés")
-    plt.savefig(nom_fichier)
-    plt.close()
